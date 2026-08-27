8dea70c6a28c1776f86200ba008bb413f6a1d51d
瓜瓜 <1224299352@qq.com>
2026-08-27 13:23:43 +0800

    删除旧版

--- goodhr5/local-agent-go旧版已废弃，请勿阅读/internal/version/version.go
+++
@@ -1,6 +0,0 @@
-// Package version 负责提供 GoodHR 本地程序版本信息。
-package version
-
-// Value 是当前本地程序版本号。
-// 构建正式包时可通过 go build -ldflags "-X goodhr5/local-agent-go/internal/version.Value=版本号" 注入。
-var Value = "5.3.5"
